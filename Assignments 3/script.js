eecd86f0ca9215af296ded8173cfc978acb443d4
yyyyy006 <105614018+yyyyy006@users.noreply.github.com>
2022-06-05 22:43:30 +0100

    Added Save and Clear Button
    Added  Save and Clear button to save and clear Local Storage

--- Assignments 3/script.js
+++ Assignments 3/script.js
@@ -1,4 +1,67 @@
-function Darknlight() {
-  var element = document.body;
-  element.classList.toggle("light-mode");
+//Function that change light to dark mode and saved to local storage
+let theme = localStorage.getItem("data-theme");
+
+const checkbox = document.getElementById("switch");
+
+const changeThemeToDark = () => {
+  document.documentElement.setAttribute("data-theme", "dark");
+  localStorage.setItem("data-theme", "dark");
+  console.log("I give you dark");
+};
+
+const changeThemeToLight = () => {
+  document.documentElement.setAttribute("data-theme", "light");
+  localStorage.setItem("data-theme", "light");
+  console.log("I give you light");
+};
+
+if (theme === "dark") {
+  changeThemeToDark();
 }
+
+checkbox.addEventListener("change", () => {
+  let theme = localStorage.getItem("data-theme");
+  if (theme === "dark") {
+    changeThemeToLight();
+  } else {
+    changeThemeToDark();
+  }
+});
+
+//Functions that stored valued from localstorage
+
+function userNameInput() {
+  var Name = document.getElementById("name").value;
+  var result = document.getElementById("result");
+
+  result.textContent = "Hello, " + "" + Name + "👋";
+
+  window.localStorage.setItem("Name", Name);
+  console.log(Name);
+}
+submitButton.addEventListener("click", userNameInput);
+
+//Functions that removes the stored valued from localstorage
+
+function deletInput() {
+  window.localStorage.removeItem("Name");
+  location.reload();
+}
+deleteButton.addEventListener("click", deletInput);
+
+// function that saved stored value from local storage on page load
+window.onload = function () {
+  let Name;
+  let result;
+
+  if (window.localStorage.getItem("Name") == null) {
+    Name = "";
+    result = "";
+  } else {
+    Name = window.localStorage.getItem("Name");
+    result = document.getElementById("result");
+    result.textContent = "Hello, " + "" + Name + "👋";
+  }
+
+  btn1();
+};
